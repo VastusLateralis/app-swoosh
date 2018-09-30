9a1d9bffafe07c5cd768a145b7788a83337a42fe
Thomas Johannessen <Silveruser@protonmail.com>
2018-09-30 14:53:59 +0200

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Thomas Johannessen on 30/09/2018.
+//  Copyright © 2018 Thomas Johannessen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
